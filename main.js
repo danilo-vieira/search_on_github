ee5a81f541cbb8b864aab5fc90b4627603fbb5c0
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-04-04 06:46:16 -0300

    Adding page

---
+++ main.js
@@ -0,0 +1,70 @@
+var listElement = document.querySelector('div#app ul');
+var inputElement = document.querySelector('div#user-tools input');
+var buttonElement = document.querySelector('div#user-tools button');
+
+function searchRepos() {
+  var github_user = inputElement.value;
+  inputElement.value = '';
+  inputElement.focus();
+
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.github.com/users/${github_user}/repos`);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject(error);
+        }
+      }
+    }
+  })
+}
+
+function renderRepos(repo_list) {
+  listElement.innerHTML = '';
+
+  for (repo of repo_list) {
+    var li = document.createElement('li');
+
+    // Título do repositório
+    var repoName = document.createTextNode('Nome do repositório: ' + repo.name);
+
+    // Descrição do repositório
+    var repoDescription = document.createTextNode('Descrição: ' + repo.description);
+
+    // Link do repositorio
+    var link = document.createElement('a');
+    var labelLink = document.createTextNode('Link para o repositório');
+    link.setAttribute('href', repo.html_url);
+    link.setAttribute('target', '_blank')
+    link.appendChild(labelLink);
+
+    var br1 = document.createElement('br');
+    var br2 = document.createElement('br');
+
+    li.appendChild(repoName);
+    li.appendChild(br1);
+
+    li.appendChild(repoDescription);
+    li.appendChild(br2);
+
+    li.appendChild(link);
+
+    listElement.appendChild(li);
+  }
+}
+
+buttonElement.onclick = 
+() => searchRepos()
+  .then(function(response) {
+    document.querySelector('div#app').classList.toggle('show');
+    var repo_list = response || [];
+    renderRepos(repo_list);
+  })
+  .catch(function(error) {
+    alert(error);
+  });
